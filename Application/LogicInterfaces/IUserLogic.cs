d632f3655c99174188f9069684c105d30e4a3b51
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2022-11-06 20:23:53 +0100

    Re upload of project

---
+++ Application/LogicInterfaces/IUserLogic.cs
@@ -0,0 +1,11 @@
+﻿using Domain.DTOs;
+using Domain.Models;
+
+namespace Application.LogicInterfaces {
+    public interface IUserLogic {
+        Task<User> CreateAsync(UserCreationDto userToCreate);
+        Task<User> GetByIdAsync(string id);
+        Task<User> GetByUserNameAsync(string userName); 
+        Task<IEnumerable<User>> GetAsync();
+    }
+}
---
+++ FileData/DAOs/UserDao.cs
@@ -0,0 +1,45 @@
+﻿using Application.DaoInterfaces;
+using Domain.Models;
+using shortid;
+using shortid.Configuration;
+
+namespace FileData.DAOs {
+    public class UserDao : IUserDao {
+        private readonly FileContext _context;
+
+        public UserDao(FileContext context) {
+            _context = context;
+        }
+
+        public Task<User> CreateAsync(User user) {
+            string newId = ShortId.Generate(new GenerationOptions(true, true, 12));
+
+            user.Id = newId;
+            user.SubscribedSubs = new List<SubPage>();
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Task.FromResult(user);
+        }
+
+        public Task<User?> GetByIdAsync(string id) {
+            User? existing = _context.Users.FirstOrDefault(u =>
+                u.Id.Equals(id)
+            );
+
+            return Task.FromResult(existing);
+        }
+
+        public Task<User?> GetByUsernameAsync(string username)
+        {
+            User? existing = _context.Users.FirstOrDefault(u =>
+                u.Username.Equals(username, StringComparison.OrdinalIgnoreCase)
+            );
+
+            return Task.FromResult(existing);
+        }
+
+
+    }
+}
